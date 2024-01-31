5273c3f5ca5350aa1a45a5f36a77229fc503ba0b
klimukviktori@gmail.com <klimukviktori@gmail.com>
2024-01-31 19:28:50 +0200

    final  body animation

--- loading.js
+++ loading.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
     setTimeout(function () {
-        document.querySelector('.preloader').style.display = 'none';
+        // document.querySelector('.preloader').style.display = 'none';
         document.querySelector('.content').style.opacity = '1';
-    }, 2000); // Задержка в 2 секунды для демонстрации
+    }, 4000); // Задержка в 2 секунды для демонстрации
 });
